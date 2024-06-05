56eb51ae1da311d573ae97c6c29855d3f71bc8a4
Roberty Rohsler <127803662+roberty0587@users.noreply.github.com>
2024-06-05 09:27:20 -0300

    Finalizado

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-06-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-06-07T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2025-01-05T00:00:00");
 
